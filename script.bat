7a74a20e1155e7905bae9c026862b91f91bdfadb
minhngkh <minhngkh@gmail.com>
2023-05-29 14:56:52 +0700

    automate EqualizerAPO installing

--- script.bat
+++ script.bat
@@ -19,9 +19,25 @@ copy "Simple Radar\01 default\*" "%csgo%\resource\overviews\" > NUL
 copy "Simple Radar\02 spectate\*" "%csgo%\resource\overviews\" > NUL
 
 :skip
+if exist "eapo.exe" (goto installed)
+
+set exe=https://onboardcloud.dl.sourceforge.net/project/equalizerapo/1.3/EqualizerAPO64-1.3.exe
+echo Downloading EqualizerAPO
+start /w curl -o eapo.exe "%exe%"
+
+:installed
+echo Intalling EqualizerAPO
+start /w eapo.exe
+
 set eapo=C:\Program Files\EqualizerAPO\config
+if not exist "%eapo%" (echo Can't find path, make sure EqualizerAPO 1.3 is properly installed)
+
 echo Applying EqualizerAPO config...
 copy "EqualizerAPO\config.txt" "%eapo%\config.txt" > NUL
+
+echo Restarting audio server
+start /w net stop audiosrv
+start /w net start audiosrv
 
 echo --- LAUNCH OPTIONS ---
 echo 128 tick: +exec autoexec.cfg -high -novid -nojoy -preload -language colormod -freq 240 -tickrate 128
